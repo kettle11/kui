659b0be84e1e08b65e63758e1d2e6483ed7b80ed
Ian Kettlewell <ian.kettlewell@gmail.com>
2020-09-27 15:14:22 -0700

    Change model of simple ui to use `kapp` async more directly

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,24 +1,25 @@
 use kui::*;
 
 fn main() {
-	run("Hello", ui);
+    run_async(ui);
 }
 
-async fn ui(context: UIContext) {	
-    let inter_medium;
-    {
-        let mut ui = context.next().await;
-        inter_medium = ui.font_from_bytes(include_bytes!("../resources/Inter-Medium.ttf"));
-    }
-    
+async fn ui(app: Application, events: Events) {
+    let mut ui = SimpleUI::new(app, events);
+    let inter_medium = ui
+        .ui
+        .font_from_bytes(include_bytes!("../resources/Inter-Medium.ttf"));
+
     let mut counter: u32 = 0;
-	loop {
-        let mut ui = context.next().await;
+
+    loop {
+        ui.update().await;
+        
         let body = ui.edit().font(inter_medium);
 
         if button(&body, id!(), "Increment counter") {
             counter += 1;
             println!("COUNTER: {:?}", counter);
         }
-	}
+    }
 }
--- src/simple_ui/mod.rs
+++ src/simple_ui/mod.rs
@@ -2,170 +2,110 @@ mod gl_drawer;
 
 use crate::UI;
 use glow::HasContext;
-use kapp::*;
-use std::cell::{RefCell, RefMut};
-use std::future::Future;
-use std::pin::Pin;
-use std::rc::Rc;
-use std::task::{Context, Poll};
+pub use kapp::*;
 
-pub fn run<F>(title: &str, run_function: impl Fn(UIContext) -> F)
-where
-    F: 'static + Future<Output = ()>,
-{
-    let (app, event_loop) = initialize();
-    let mut window_width = 1200;
-    let mut window_height = 800;
-    let window = app
-        .new_window()
-        .title(title)
-        .size(window_width, window_height)
-        .build()
-        .unwrap();
+pub struct SimpleUI {
+    pub ui: UI,
+    application: Application,
+    events: Events,
+    window: Window,
+    gl_context: GLContext,
+    gl: glow::Context,
+    gl_drawer: gl_drawer::GLDrawer,
+}
 
-    // Create a GLContext
-    let mut gl_context = GLContext::new().samples(4).build().unwrap();
-    gl_context.set_window(Some(&window)).unwrap();
-
-    #[cfg(target_arch = "wasm32")]
-    let gl = glow::Context::from_webgl2_context(gl_context.webgl2_context().unwrap());
-    #[cfg(not(target_arch = "wasm32"))]
-    let gl = glow::Context::from_loader_function(|s| gl_context.get_proc_address(s));
-
-    unsafe {
-        gl.viewport(0, 0, window_width as i32, window_height as i32);
+impl SimpleUI {
+    pub fn edit(&mut self) -> crate::UIBuilder {
+        self.ui.edit()
     }
 
-    let ui_context = UIContext::new();
+    pub fn new(app: Application, events: Events) -> Self {
+        let window_width = 1200;
+        let window_height = 800;
 
-    ui_context
-        .get_ui()
-        .resize(window_width as f32, window_height as f32);
+        let window = app
+            .new_window()
+            .title("Hello")
+            .size(window_width, window_height)
+            .build()
+            .unwrap();
 
-    let mut gl_drawer = gl_drawer::GLDrawer::new(&gl);
+        // Create a GLContext
+        let mut gl_context = GLContext::new().samples(4).build().unwrap();
+        gl_context.set_window(Some(&window)).unwrap();
 
-    // Run user function for the first time
-    let mut user_future = Box::pin(run_function(ui_context.clone()));
-    let waker = empty_waker::create();
-    let mut context = Context::from_waker(&waker);
-    let _ = user_future.as_mut().poll(&mut context);
+        #[cfg(target_arch = "wasm32")]
+        let gl =
+            unsafe { glow::Context::from_webgl2_context(gl_context.webgl2_context().unwrap()) };
+        #[cfg(not(target_arch = "wasm32"))]
+        let gl = unsafe { glow::Context::from_loader_function(|s| gl_context.get_proc_address(s)) };
 
-    window.request_redraw();
-    event_loop.run(move |event| match event {
-        Event::WindowCloseRequested { .. } => app.quit(),
-        Event::WindowResized { width, height, .. } => {
-            gl_context.resize();
-            unsafe {
-                gl_context.resize(); // Resizes the window buffer
-                gl.viewport(0, 0, width as i32, height as i32);
-            }
-            ui_context.get_ui().resize(width as f32, height as f32);
-            window.request_redraw();
+        unsafe {
+            gl.viewport(0, 0, window_width as i32, window_height as i32);
         }
-        Event::PointerMoved { x, y, .. } => {
-            ui_context.get_ui().pointer_move(x as f32, y as f32);
-            window.request_redraw();
-        }
-        Event::PointerDown { x, y, .. } => {
-            ui_context.get_ui().pointer_down(x as f32, y as f32);
-            window.request_redraw();
-        }
-        Event::PointerUp { x, y, .. } => {
-            ui_context.get_ui().pointer_up(x as f32, y as f32);
-            window.request_redraw();
-        }
-        Event::Scroll { delta_y, .. } => {
-            ui_context.get_ui().scroll(delta_y as f32);
-            window.request_redraw();
-        }
-        Event::Draw { .. } => {
-            // ----------- Build UI ---------------
+        let gl_drawer = gl_drawer::GLDrawer::new(&gl);
 
-            // Call into user async function giving it a chance to update.
-            let waker = empty_waker::create();
-            let mut context = Context::from_waker(&waker);
-            *ui_context.ready.borrow_mut() = true;
-            let _ = user_future.as_mut().poll(&mut context);
+        window.request_redraw();
 
-            {
-                let mut ui_borrow = ui_context.get_ui();
-                let drawables = ui_borrow.render();
-                gl_drawer.draw(&gl, &drawables);
-            }
-            if ui_context.get_ui().needs_redraw() {
-                window.request_redraw();
-            }
+        let mut ui = UI::new();
 
-            gl_context.swap_buffers();
-        }
-        _ => {}
-    });
-}
+        ui.resize(window_width as f32, window_height as f32);
 
-#[derive(Clone)]
-pub struct UIContext {
-    ui: Rc<RefCell<UI>>,
-    ready: Rc<RefCell<bool>>,
-}
-
-impl UIContext {
-    pub fn new() -> Self {
+        // Move all initial setup to here.
         Self {
-            ui: Rc::new(RefCell::new(UI::new())),
-            ready: Rc::new(RefCell::new(true)),
+            ui,
+            application: app,
+            events,
+            gl_context,
+            window,
+            gl,
+            gl_drawer,
         }
     }
 
-    pub fn next(&self) -> self::NextFrameFuture {
-        self::NextFrameFuture {
-            ui_context: self,
-            phantom: std::marker::PhantomData,
+    pub async fn update(&mut self) {
+        // Perform drawing here assuming that a break occurred at a draw previously.
+        let drawing_info = self.ui.render();
+        self.gl_drawer.draw(&self.gl, &drawing_info);
+
+        if self.ui.needs_redraw() {
+            self.window.request_redraw();
         }
-    }
+        self.gl_context.swap_buffers();
 
-    pub(crate) fn get_ui(&self) -> RefMut<UI> {
-        (*self.ui).borrow_mut()
-    }
-}
-
-pub struct NextFrameFuture<'a, 'b> {
-    ui_context: &'a UIContext,
-    phantom: std::marker::PhantomData<&'b ()>,
-}
-
-impl<'a: 'b, 'b> Future for NextFrameFuture<'a, 'b> {
-    type Output = RefMut<'b, UI>;
-
-    fn poll(self: Pin<&mut Self>, _ctx: &mut Context) -> Poll<Self::Output> {
-        if *self.ui_context.ready.borrow() {
-            *self.ui_context.ready.borrow_mut() = false;
-            if let Ok(ui) = (*self.ui_context.ui).try_borrow_mut() {
-                Poll::Ready(ui)
-            } else {
-                Poll::Pending
+        loop {
+            match self.events.next().await {
+                Event::WindowCloseRequested { .. } => self.application.quit(),
+                Event::WindowResized { width, height, .. } => {
+                    self.gl_context.resize();
+                    unsafe {
+                        self.gl_context.resize(); // Resizes the window buffer
+                        self.gl.viewport(0, 0, width as i32, height as i32);
+                    }
+                    self.ui.resize(width as f32, height as f32);
+                    self.window.request_redraw();
+                }
+                Event::PointerMoved { x, y, .. } => {
+                    self.ui.pointer_move(x as f32, y as f32);
+                    self.window.request_redraw();
+                }
+                Event::PointerDown { x, y, .. } => {
+                    self.ui.pointer_down(x as f32, y as f32);
+                    self.window.request_redraw();
+                }
+                Event::PointerUp { x, y, .. } => {
+                    self.ui.pointer_up(x as f32, y as f32);
+                    self.window.request_redraw();
+                }
+                Event::Scroll { delta_y, .. } => {
+                    self.ui.scroll(delta_y as f32);
+                    self.window.request_redraw();
+                }
+                Event::Draw { .. } => {
+                    return;
+                }
+                _ => {}
             }
-        } else {
-            Poll::Pending
         }
     }
-}
-
-/// This should be expanded in the future in case the user wants to wait on other
-/// async events.
-mod empty_waker {
-    use std::task::{RawWaker, RawWakerVTable, Waker};
-
-    pub fn create() -> Waker {
-        unsafe { Waker::from_raw(RAW_WAKER) }
-    }
-
-    const RAW_WAKER: RawWaker = RawWaker::new(std::ptr::null(), &VTABLE);
-    const VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, drop);
-
-    unsafe fn clone(_: *const ()) -> RawWaker {
-        RAW_WAKER
-    }
-    unsafe fn wake(_: *const ()) {}
-    unsafe fn wake_by_ref(_: *const ()) {}
-    unsafe fn drop(_: *const ()) {}
 }
